d1d136d3e6fcc9c1a5761ef8aea1a9453444b414
omaima <benhlima.omaima.solicode@gmail.com>
2024-10-08 11:51:01 +0100

    add products cart

---
+++ src/Products.js
@@ -0,0 +1,14 @@
+import image1 from './assets/images/1.png';
+import image2 from "./assets/images/2 (2).png";
+import image3 from "./assets/images/3.png";
+import image4 from "./assets/images/4.png";
+
+
+export const products = [{ id: 1, name: "Hertfoid Upholstered Chair", price: 101, image: image1, description: "Hertfoid Upholstered Chair", slug: "hertfoid-upholstered-chair" },
+    { id: 2, name: "Hertfoid Upholstered Chair", price: 101, image: image2, description: "Hertfoid Upholstered Chair", slug: "hertfoid-upholstered-chair" },
+    { id: 3, name: "Hertfoid Upholstered Chair", price: 101, image: image3, description: "Hertfoid Upholstered Chair", slug: "hertfoid-upholstered-chair" },
+    { id: 4, name: "Hertfoid Upholstered Chair", price: 101, image: image4, description: "Hertfoid Upholstered Chair", slug: "hertfoid-upholstered-chair" }];
+
+
+    //slug is a part of url  that identifies a specific page on 
+    //a website in a way that's easy to read for both humans and search engines.
---
+++ src/components/CartTab.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CartTab = () => {
+  return (
+    <div>
+      cart 
+    </div>
+  )
+}
+
+export default CartTab
